1074833426836bbb326311c1d4ca694ab0cbdebe
beltonhe <beltonhe@github.com>
2019-10-30 17:53:01 -0600

    worked on by Atilla and Belton

--- MoodSoup/app/src/main/java/com/example/test/moodsoup/Profile.java
+++ MoodSoup/app/src/main/java/com/example/test/moodsoup/Profile.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,7 +15,7 @@ public class Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
-        final Button toFollowing = findViewById(R.id.button);
+        final ImageButton toFollowing = findViewById(R.id.imageButton);
         toFollowing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
