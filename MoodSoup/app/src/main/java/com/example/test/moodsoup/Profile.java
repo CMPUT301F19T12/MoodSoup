fa8c6d9b6aac088053540b83218343e55156fe89
beltonhe <beltonhe@github.com>
2019-11-01 17:52:34 -0600

    updated by Belton and Atilla

--- MoodSoup/app/src/main/java/com/example/test/moodsoup/Profile.java
+++ MoodSoup/app/src/main/java/com/example/test/moodsoup/Profile.java
@@ -3,17 +3,34 @@ package com.example.test.moodsoup;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.ListView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+import io.opencensus.metrics.export.Summary;
+
 public class Profile extends AppCompatActivity {
+    private ListView moodList;
+    private ArrayList<String> moods = new ArrayList<String>();
+    private ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         final ImageButton toFollowing = findViewById(R.id.imageButton);
         toFollowing.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +40,22 @@ public class Profile extends AppCompatActivity {
                 startActivity(FollowingIntent);
             }
         });
+
+        db.collection("Users").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                for (DocumentSnapshot snapshot : queryDocumentSnapshots){
+                    moods.add(snapshot.getString("date"));
+                    moods.add(snapshot.getString("emotions"));
+                    moods.add(snapshot.getString("location"));
+                    moods.add(snapshot.getString("reason"));
+                    moods.add(snapshot.getString("social"));
+                    moods.add(snapshot.getString("time"));
+                }
+            }
+        });
     }
+
+
+
 }
