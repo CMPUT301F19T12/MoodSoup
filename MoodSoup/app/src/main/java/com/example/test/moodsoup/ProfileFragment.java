b808b4bfc93dc6ee63b633323ba03d6a7e22c0d1
beltonhe <beltonhe@github.com>
2019-11-05 22:06:18 -0700

    work in progress by Belton and Atilla

--- MoodSoup/app/src/main/java/com/example/test/moodsoup/ProfileFragment.java
+++ MoodSoup/app/src/main/java/com/example/test/moodsoup/ProfileFragment.java
@@ -1,9 +1,33 @@
 package com.example.test.moodsoup;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.content.Context;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +35,76 @@ import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
 
-    @Nullable
-    @Override
+    private ListView moodList;
+    private TextView profileName;
+    private ArrayList<String> mood = new ArrayList<>();
+    private ArrayAdapter<String> moodAdapter;
+    private FirebaseAuth mAuth;
+
+    private ImageButton toFollowing;
+    private ImageButton imageButton2;
+    private static int RESULT_LOAD_IMG = 1;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile,container,false);
+        final View root = inflater.inflate(R.layout.profile,container,false);
+        ///Set profile picture
+        final ImageButton button = (ImageButton)root.findViewById(R.id.imageButton2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button.setSelected(!button.isPressed());
+
+                if (button.isPressed()) {
+
+                    Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                    photoPickerIntent.setType("image/*");
+                    startActivityForResult(photoPickerIntent, RESULT_LOAD_IMG);
+
+
+                    button.setImageResource(R.drawable.moodsoup_happy);
+                } else {
+                    button.setImageResource(R.drawable.moodsoup_sad);
+                }
+            }
+        });
+
+        // View ID
+        profileName = root.findViewById(R.id.ProfileName);
+        moodList = root.findViewById(R.id.event_list_self);
+        toFollowing = root.findViewById(R.id.imageButton);
+
+        // User Instance
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        // variables
+        String uid = user.getUid();
+        String name = user.getDisplayName();
+        String email = user.getEmail();
+
+
+        // Set user name on profile layout display view
+        final TextView display_name = profileName;
+        display_name.setText("User email: " + email);
+
+        //pulling data from the  /////////////
+        CollectionReference moodRef = db.collection("Users").document(email).collection("moodHistory");
+        moodRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        mood.add(document.getId());
+                    }
+                    // moodAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,mood);
+                    moodList.setAdapter(moodAdapter);
+                }
+            }
+        });
+
+            return root;
+        }
     }
-}
+
